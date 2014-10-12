92fe8c11bd57c0178a69e94222f51ed766dc42a1
Walee <wqawasmi@gmail.com>
2014-10-12 03:57:24 -0400

    Update Game.java
    Changed player1, player2, rock, paper and scissors variables to INTEGERS

--- Game.java
+++ Game.java
@@ -1,13 +1,19 @@
 public class Game {
     
     //Game vars
-    public static String player1;
-    public static String player2;
+    public static int player1;
+    public static int player2;
     public static boolean gameStart = false;
     
-    private String paper = "PAPER\n";
-    private String rock  = "ROCK\n";
-    private String scissors = "SCISSORS\n";
+    /*
+    private String paper = "PAPER";
+    private String rock  = "ROCK";
+    private String scissors = "SCISSORS";
+    */
+    
+    private final static int rock = 1;
+    private final static int paper = 2;
+    private final static int scissors = 3;
     
     public Game() {
         //constructor shiz
@@ -15,7 +21,7 @@ public class Game {
     
     
     public static boolean gameReady() {
-        if(player1 != null && player2 != null) 
+        if(player1 != 0 && player2 != 0) 
             gameStart = true;
         else
             gameStart = false;
@@ -23,40 +29,40 @@ public class Game {
     }
     
     public static void resetGame() {
-        player1 = null;
-        player2 = null;
+        player1 = 0;
+        player2 = 0;
     }
     
     public static void processGestures() {
-        System.out.println("Player one: " + player1);
+        System.out.println("Player one: " + player1 + "shite");
         System.out.println("Player two: " + player2);
         
         
-        if(player1.equals(player2)) {
+        if(player1 == player2) {
             System.out.println("TIE!!!!");
             //System.out.print(player1);
         } 
-        else if(player1.equals(rock) && player2.equals(scissors)){
+        else if(player1 == rock && player2 == scissors){
             System.out.println("Rock CRUSHES Scissors!!!");
         }
-        else if(player1.equals(rock) && player2.equals(paper)) {
+        else if(player1 == rock && player2 == paper) {
             System.out.println("Paper COVERS Rock!!!");
         } 
-        else if(player1.equals(paper) && player2.equals(scissors)) {
+        else if(player1 == paper && player2 == scissors) {
             System.out.println("Scissors CUTS Paper!!!");
         }
-        else if(player1.equals(paper) && player2.equals(rock)) {
+        else if(player1 == paper && player2 == rock) {
             System.out.println("Paper COVERS Rock!!!");
         }
-        else if(player1.equals(scissors) && player2.equals(rock)) {
+        else if(player1 == scissors && player2 == rock) {
             System.out.println("Rock CRUSHES Scissors!!!");
         }
-        else if(player1.equals(scissors) && player2.equals(paper)) {
+        else if(player1 == scissors && player2 == paper) {
             System.out.println("Scissors CUTS Paper!!!");
         }
         
-        player1 = null;
-        player2 = null;
+        player1 = 0;
+        player2 = 0;
     }
     
     
